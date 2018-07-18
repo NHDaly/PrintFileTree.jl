d854a2d5b4f39fc3507bbae2f12da054bf1c9931
Nathan Daly <nhdaly@gmail.com>
2018-07-17 20:47:14 -0400

    Add `printfiletree(root)` (like unix `tree`)

--- src/PrintFileTree.jl
+++ src/PrintFileTree.jl
@@ -1,5 +1,35 @@
 module PrintFileTree
 
-# package code goes here
+export printfiletree
+
+"""
+    printfiletree(root)
+
+Like the unix utility `tree`.
+"""
+function printfiletree(root)
+    println(root);
+    printfiletree_helper(root)
+end
+function printfiletree_helper(root, depth=0, opendirs=[true])
+    files = readdir(root)
+    for (i,f) in enumerate(files)
+        startswith(f, ".") && continue
+        lastitem = (i == length(files))
+        lastitem && (opendirs[end] = false)
+        for p in opendirs[1:end-1]
+            print(p ? "│   " : "    ")
+        end
+        println("$(lastitem ? "└" : "├")── " * f) # path
+        path = joinpath(root, f)
+
+        if isdir(path)
+            push!(opendirs, true)
+            printfiletree_helper(path, depth+1, opendirs)
+            pop!(opendirs)
+        end
+    end
+    return length(files)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,32 @@
 using PrintFileTree
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+using Base.Test
 
-# write your own tests here
-@test 1 == 2
+tmpdir = mktempdir()
+touch(joinpath(tmpdir, "a"))
+touch(joinpath(tmpdir, "b"))
+mkpath(joinpath(tmpdir, "c","a","a"))
+touch(joinpath(tmpdir, "c", "cool"))
+mkpath(joinpath(tmpdir, "c", "cats", "yeah", "so", "cool"))
+touch(joinpath(tmpdir, "c", "a", "subfiles"))
+touch(joinpath(tmpdir, "c", "a", "a","subfiles"))
+mkpath(joinpath(tmpdir, "c","a","a"))
+mkpath(joinpath(tmpdir, "c","a","a"))
+touch(joinpath(tmpdir, "d"))
+
+# Expect the following output:
+"""$tmpdir
+├-- a
+├-- b
+├-- c
+│   ├-- a
+│   │   ├-- a
+│   │   │   └-- subfiles
+│   │   └-- subfiles
+│   ├-- cats
+│   │   └-- yeah
+│   │       └-- so
+│   │           └-- cool
+│   └-- cool
+└-- d
+"""
+printfiletree(tmpdir)
