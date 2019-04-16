448b23ba6bc5e7e058e69b566531fb79aae383f6
Nathan Daly <nhdaly@gmail.com>
2019-04-16 01:28:45 -0400

    Add directories and files counts to print statement
    Mirroring the output from the $ tree command

--- src/PrintFileTree.jl
+++ src/PrintFileTree.jl
@@ -12,9 +12,10 @@ Prints complete recursive directory structure of root and all its contents.
 """
 function printfiletree(root=".")
     println(root);
-    printfiletree_helper(root)
+    d,f = printfiletree_helper(root)
+    println("\n$d directories, $f files")
 end
-function printfiletree_helper(root, depth=0, opendirs=[true])
+function printfiletree_helper(root, depth=0, opendirs=[true], dirscount=fill(0), filescount=fill(0))
     files = readdir(root)
     for (i,f) in enumerate(files)
         startswith(f, ".") && continue
@@ -27,12 +28,15 @@ function printfiletree_helper(root, depth=0, opendirs=[true])
         path = joinpath(root, f)
 
         if isdir(path)
+            dirscount[] += 1
             push!(opendirs, true)
-            printfiletree_helper(path, depth+1, opendirs)
+            printfiletree_helper(path, depth+1, opendirs, dirscount, filescount)
             pop!(opendirs)
+        else
+            filescount[] += 1
         end
     end
-    nothing
+    dirscount[], filescount[]
 end
 
 end # module
